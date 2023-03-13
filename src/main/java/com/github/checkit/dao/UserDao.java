d145fd7e95248ce2639bec8d76b5740a1342d821
Michal Švagr <michalsvag@gmail.com>
2023-03-13 18:36:00 +0100

    UserDao removing dead code

--- src/main/java/com/github/checkit/dao/UserDao.java
+++ src/main/java/com/github/checkit/dao/UserDao.java
@@ -1,25 +1,12 @@
 package com.github.checkit.dao;
 
 import com.github.checkit.model.User;
-import com.github.checkit.util.TermVocabulary;
 import cz.cvut.kbss.jopa.model.EntityManager;
-import java.net.URI;
-import java.util.HashSet;
-import java.util.Set;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserDao extends BaseDao<User> {
     protected UserDao(EntityManager em) {
         super(User.class, em);
-    }
-
-    private Set<URI> loadGestoredVocabularies(User user) {
-        return new HashSet<>(em.createNativeQuery("SELECT ?vocab WHERE {"
-                + "?user ?jeGestorem ?vocab ."
-                + "}", URI.class)
-            .setParameter("user", user.getUri())
-            .setParameter("jeGestorem", URI.create(TermVocabulary.s_p_je_gestorem))
-            .getResultList());
     }
 }
