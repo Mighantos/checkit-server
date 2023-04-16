9e9bd6990d3663e23e2a52fb3a74b5957007a590
Michal Švagr <michalsvag@gmail.com>
2023-04-16 21:13:30 +0200

    Comments show tag

--- src/main/java/com/github/checkit/dto/CommentDto.java
+++ src/main/java/com/github/checkit/dto/CommentDto.java
@@ -1,6 +1,7 @@
 package com.github.checkit.dto;
 
 import com.github.checkit.model.Comment;
+import com.github.checkit.model.auxilary.CommentTag;
 import java.net.URI;
 import java.time.Instant;
 import lombok.Getter;
@@ -12,6 +13,7 @@ public class CommentDto {
     private final UserDto author;
     private final Instant creationDate;
     private final Instant lastModificationDate;
+    private final CommentTag tag;
     private final String content;
 
     /**
@@ -23,6 +25,7 @@ public class CommentDto {
         this.author = new UserDto(comment.getAuthor());
         this.creationDate = comment.getCreated();
         this.lastModificationDate = comment.getModified();
+        this.tag = comment.getTag();
         this.content = comment.getContent();
     }
 }
