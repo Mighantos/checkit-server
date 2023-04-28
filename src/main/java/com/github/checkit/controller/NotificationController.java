0ff29e5ea8f24efa8872b50c19ecc2291f266712
Michal Švagr <michalsvag@gmail.com>
2023-04-28 12:11:53 +0200

    Change notification URI from param to body

--- src/main/java/com/github/checkit/controller/NotificationController.java
+++ src/main/java/com/github/checkit/controller/NotificationController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -37,7 +38,7 @@ public class NotificationController extends BaseController {
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PostMapping("/seen")
-    public void markSeen(@RequestParam URI notificationUri) {
+    public void markSeen(@RequestBody URI notificationUri) {
         notificationService.markSeen(notificationUri);
     }
 }
